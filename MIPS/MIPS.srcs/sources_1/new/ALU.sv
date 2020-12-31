0cf0041e01ebc7fb5eb1d640b4317d93930b6ee3
Waseem <waseem7446@gmail.com>
2020-12-30 21:31:01 -0500

    added register file

--- MIPS/MIPS.srcs/sources_1/new/ALU.sv
+++ MIPS/MIPS.srcs/sources_1/new/ALU.sv
@@ -21,77 +21,69 @@ module ALU(
     output  logic    [31:0]  o_output
     );
     
-always_comb
-begin
-    if (i_reg_not_imm == 1'b1)
-        case (i_opcode)
-            6'b100000:      // add
-                o_output = i_operand1 + i_operand2;
-            6'b100001:      // addu
-                o_output = i_operand1 + i_operand2;
-            6'b100100:      // and
-                o_output = i_operand1 & i_operand2;
-            6'b100111:      // nor
-                o_output = ~(i_operand1 | i_operand2);
-            6'b100101:      // or
-                o_output = i_operand1 | i_operand2;
-            6'b000000:      // sll
-                o_output = i_operand2 << i_shamt;
-            6'b000100:      // sllv
-                o_output = i_operand2 << i_operand1;
-            6'b000011:      // sra
-                o_output = signed'(i_operand2) >>> i_shamt;
-            6'b000111:      // srav
-                o_output = signed'(i_operand2) >>> i_operand1;
-            6'b000010:      // srl
-                o_output = i_operand2 >> i_shamt;
-            6'b000110:      // srlv
-                o_output = i_operand2 >> i_operand1;
-            6'b100010:      // sub
-                o_output = i_operand1 - i_operand2;
-            6'b100011:      // subu
-                o_output = i_operand1 - i_operand2;
-            6'b100110:      // xor
-                o_output = i_operand1 ^ i_operand2;
-            6'b101010:      // slt
-                o_output = (signed'(i_operand1) < signed'(i_operand2)) ? 32'h00000001 : 0;
-            6'b101001:      // sltu
-                o_output = (i_operand1 < i_operand2) ? 32'h00000001 : 0;
-            default:
-                o_output = 0;
-        endcase
-    else
-        case (i_opcode)
-            6'b001000:      // addi (sign extended i)
-                o_output = i_operand1 + i_operand2;
-            6'b001001:      // addiu (sign extended i)
-                o_output = i_operand1 + i_operand2;
-            6'b001100:      // andi (zero extended i)
-                o_output = i_operand1 & i_operand2 & 32'h0000ffff;
-            6'b001101:      // ori (zero extended i)
-                o_output = i_operand1 | (i_operand2 & 32'h0000ffff);
-            6'b001110:      // xori (zero extended i)
-                o_output = i_operand1 ^ (i_operand2 & 32'h0000ffff);
-            6'b011001:      // lui (load upper 16 bits)
-                o_output = {i_operand2, 16'h0000};
-            6'b001010:      // slti (sign extended i)
-                o_output = (signed'(i_operand1) < signed'(i_operand2)) ? 32'h00000001 : 0;
-            6'b001001:      // sltiu (sign extended i)
-                o_output = (i_operand1 < i_operand2) ? 32'h00000001 : 0;
-            6'b100011:      // lw (sign extended i)
-                o_output = i_operand1 + i_operand2;
-            6'b101011:      // sw (sign extended i)
-                o_output = i_operand1 + i_operand2;
-            default:
-                o_output = 0;
-        endcase
-end
-            
-            
-            
-            
-            
-            
-            
-            
+    always_comb
+    begin
+        if (i_reg_not_imm == 1'b1)
+            case (i_opcode)
+                6'b100000:      // add
+                    o_output = i_operand1 + i_operand2;
+                6'b100001:      // addu
+                    o_output = i_operand1 + i_operand2;
+                6'b100100:      // and
+                    o_output = i_operand1 & i_operand2;
+                6'b100111:      // nor
+                    o_output = ~(i_operand1 | i_operand2);
+                6'b100101:      // or
+                    o_output = i_operand1 | i_operand2;
+                6'b000000:      // sll
+                    o_output = i_operand2 << i_shamt;
+                6'b000100:      // sllv
+                    o_output = i_operand2 << i_operand1;
+                6'b000011:      // sra
+                    o_output = signed'(i_operand2) >>> i_shamt;
+                6'b000111:      // srav
+                    o_output = signed'(i_operand2) >>> i_operand1;
+                6'b000010:      // srl
+                    o_output = i_operand2 >> i_shamt;
+                6'b000110:      // srlv
+                    o_output = i_operand2 >> i_operand1;
+                6'b100010:      // sub
+                    o_output = i_operand1 - i_operand2;
+                6'b100011:      // subu
+                    o_output = i_operand1 - i_operand2;
+                6'b100110:      // xor
+                    o_output = i_operand1 ^ i_operand2;
+                6'b101010:      // slt
+                    o_output = (signed'(i_operand1) < signed'(i_operand2)) ? 32'h00000001 : 0;
+                6'b101001:      // sltu
+                    o_output = (i_operand1 < i_operand2) ? 32'h00000001 : 0;
+                default:
+                    o_output = 0;
+            endcase
+        else
+            case (i_opcode)
+                6'b001000:      // addi (sign extended i)
+                    o_output = i_operand1 + i_operand2;
+                6'b001001:      // addiu (sign extended i)
+                    o_output = i_operand1 + i_operand2;
+                6'b001100:      // andi (zero extended i)
+                    o_output = i_operand1 & i_operand2 & 32'h0000ffff;
+                6'b001101:      // ori (zero extended i)
+                    o_output = i_operand1 | (i_operand2 & 32'h0000ffff);
+                6'b001110:      // xori (zero extended i)
+                    o_output = i_operand1 ^ (i_operand2 & 32'h0000ffff);
+                6'b011001:      // lui (load upper 16 bits)
+                    o_output = {i_operand2, 16'h0000};
+                6'b001010:      // slti (sign extended i)
+                    o_output = (signed'(i_operand1) < signed'(i_operand2)) ? 32'h00000001 : 0;
+                6'b001001:      // sltiu (sign extended i)
+                    o_output = (i_operand1 < i_operand2) ? 32'h00000001 : 0;
+                6'b100011:      // lw (sign extended i)
+                    o_output = i_operand1 + i_operand2;
+                6'b101011:      // sw (sign extended i)
+                    o_output = i_operand1 + i_operand2;
+                default:
+                    o_output = 0;
+            endcase
+    end       
 endmodule
---
+++ MIPS/MIPS.srcs/sources_1/new/register_file.sv
@@ -0,0 +1,38 @@
+/*--------------------------------------------------------------------------------
+Author: Waseem Orphali 
+Create Date: 12/28/2020
+Module Name: Register File
+Project Name: MIPS
+Target Devices: ARTY Z20
+Description: 
+The register file holds 32 general purpose registers R0 - R31 that can be read
+and written to using the addresses of the registers 00000 - 11111.
+Register R0 holds 32'h00000000 and cannot be written to.
+
+--------------------------------------------------------------------------------*/
+module register_file(
+    input   logic   [4:0]   i_r_reg1,
+    input   logic   [4:0]   i_r_reg2,
+    input   logic   [4:0]   i_w_reg,
+    input   logic           i_write,
+    input   logic   [31:0]  i_w_data,
+    output  logic   [31:0]  o_reg1,
+    output  logic   [31:0]  o_reg2
+    );
+    
+    logic   [31:0]  register [0:31] = '{default : 0};
+    
+    always_latch
+    begin
+        register[0] = 32'h00000000;             // R0 = 0 and cannot be modified
+        if (i_write && (i_w_reg != 5'b00000))
+            register [i_w_reg] = i_w_data;
+        o_reg1 = register [i_r_reg1];
+        o_reg2 = register [i_r_reg2];
+    end
+    
+    
+    
+    
+    
+endmodule
